5128bd7f910dccd5c5b9ad8ba82f4c9ad17c8b14
Paul Sisson <paulsissonsemail@gmail.com>
2023-01-08 13:39:00 -0600

    updated mode js

--- main.js
+++ main.js
@@ -41,67 +41,51 @@
 //   return mean;
 // };
 
-//console.log(meanFinder(array1));
 
-// const object1 = {
-//   1: 1,
-//   2: 2,
-//   3: 2,
-//   4: 1,
-//   5: 2,
-//   6: 1,
-//   7: 1
-// };
-
-
+const array1 = [100, 200, 300, 300, 200, 300, 200, 666, 700];
+const array2 = [200, 300]
+let allModes = [];
 const count = {};
+let trueMode = 0;
+
 const modeCount = (array) => {
   array.forEach(function(x) {count[x] = (count[x] || 0) + 1;});       //counting the occurrences in the array                                  
   return count;
 };
 
-// const modeFinder = (object) => {
-//   const mode = Math.max(...Object.values(object));
-//   const allModes = [];
-//   for (const [key, value] of Object.entries(object)) {
-//     if (value === mode) {                                             //finding the mode/modes
-//       allModes.push(key);
-//     } 
-//   } console.log(allModes);
-//   return allModes;
-// };
-
-// //modeFinder(object1);
-// const array1 = [1, 2, 3, 3, 2, 4, 5, 6, 7];
-// const allModes = [];
-
-// const firstMode = (array, array2) => {          
-//   let lowest = array.length;
-//   for (let i = 0; i < array.length; i++) {                             //ensuring the mode is the true mode
-//     if (array.lastIndexOf(array2[i]) < lowest) {
-//       lowest = array2[i];
-//     }
-//   } console.log(lowest);
-// };
-
-// const findMode = (array) => {
-//   modeCount(array);
-//   modeFinder(count);
-//   firstMode(array1, allModes);
-// };
-
-// findMode(array1);
-
-
-// if array1[i] === array2[i] {
-//   array1.lastIndexOf(array1[i])
-// }
-
-const array4 = [1, 2, 3, 6, 6, 6, 6, 6, 8];
-
-const testing = (array) => {
-  for (let i = 0; i < array.length; i++) {
-  console.log(array.lastIndexOf(array[i]))
-  }
+const modeFinder = (object) => {
+  const mode = Math.max(...Object.values(object));
+  for (const [key, value] of Object.entries(object)) {
+    if (value === mode) {                                             //finding the mode/modes
+      allModes.push(key);
+    } 
+  } return allModes;
 };
-testing(array4);
+
+//findMode(array1);
+
+function compareNumbers(a, b) {
+  return a - b;
+};
+//idea: loop through the array of modes and get the last index of i in the original array
+
+const firstMode = (array, array2) => {
+  let modeArray = [];
+  array2.forEach(i => modeArray.push(array.lastIndexOf(i)));
+  modeArray.sort(compareNumbers);
+  let trueModeIndex = modeArray[0];
+  let trueMode = array[trueModeIndex]
+  console.log(trueMode);
+  return trueMode;
+};
+firstMode(array1, array2)
+  // const findMode = (array) => {
+  //   modeCount(array);
+  //   modeFinder(count);
+  //   console.log(allModes);
+  //   console.log(array);
+  //   firstMode(array, allModes);
+  //   console.log(trueMode);
+  // };
+
+  findMode(array1, array2);
